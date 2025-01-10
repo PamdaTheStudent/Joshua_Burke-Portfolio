8e976b45ab0512c9acd0bd10d5f218904eeb86bc
PamdaTheStudent <bur21032@byui.edu>
2025-01-10 12:56:16 -0700

    Update HelloWorld-Python.py

--- HelloWorld-Python.py
+++ HelloWorld-Python.py
@@ -1,1 +1,8 @@
-print("Hello World")
+#Defining the Main program
+def main():
+    #then the main program prints the text into the console for the user to see
+    print("Hello World")
+
+#we check if the script is being run directly and if so then we run the main program
+if __name__ == "__main__":
+    main()
